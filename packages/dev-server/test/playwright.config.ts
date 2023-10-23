b74828ec5d60c1da398e03b7d9401aeb344ce497
Yusuke Wada <yusuke@kamawada.com>
2023-10-24 07:08:41 +0900

    chore: switch to `yarn`

--- packages/dev-server/test/playwright.config.ts
+++ packages/dev-server/test/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'pnpm exec vite --port 6173 -c ./test/vite.config.ts',
+    command: 'yarn vite --port 6173 -c ./vite.config.ts',
     port: 6173,
     reuseExistingServer: !process.env.CI,
   },
