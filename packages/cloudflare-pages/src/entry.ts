8c452d62b3ed2541887082c6553ee90d5b3f242a
Yusuke Wada <yusuke@kamawada.com>
2024-01-25 23:34:08 +0900

    fix(pages): fix default entry path and throw error if not found

--- packages/cloudflare-pages/src/entry.ts
+++ packages/cloudflare-pages/src/entry.ts
@@ -5,15 +5,20 @@ export type Options = {
 }
 
 export const getEntryContent = async (options: Options) => {
-  const appStr = `const modules = import.meta.glob([${options.entry
-    .map((e) => `'/${normalize(e)}'`)
-    .join(',')}], { import: 'default', eager: true })
+  const globStr = options.entry.map((e) => `'/${normalize(e)}'`).join(',')
+  const appStr = `const modules = import.meta.glob([${globStr}], { import: 'default', eager: true })
+      let added = false
       for (const [, app] of Object.entries(modules)) {
         if (app) {
           worker.route('/', app)
           worker.notFound(app.notFoundHandler)
+          added = true
         }
-      }`
+      }
+      if (!added) {
+        throw new Error("Can't import modules from [${globStr}]")
+      }
+      `
 
   return `import { Hono } from 'hono'
 import { serveStatic } from 'hono/cloudflare-pages'
