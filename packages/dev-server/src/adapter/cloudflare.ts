5195c6d2355d0bf11f481b950bcc120747fbf93b
Yusuke Wada <yusuke@kamawada.com>
2024-03-10 21:11:29 +0900

    allow receving proxy options

--- packages/dev-server/src/adapter/cloudflare.ts
+++ packages/dev-server/src/adapter/cloudflare.ts
@@ -1,10 +1,16 @@
 import { getPlatformProxy } from 'wrangler'
 import type { Adapter, Env } from '../types.js'
 
+type CloudflareAdapterOptions = {
+  proxy: Parameters<typeof getPlatformProxy>[0]
+}
+
 let proxy: Awaited<ReturnType<typeof getPlatformProxy<Env>>>
 
-export const cloudflareAdapter: () => Promise<Adapter> = async () => {
-  proxy ??= await getPlatformProxy()
+export const cloudflareAdapter: (options?: CloudflareAdapterOptions) => Promise<Adapter> = async (
+  options
+) => {
+  proxy ??= await getPlatformProxy(options?.proxy)
   return {
     env: proxy.env,
     onServerClose: proxy.dispose,
