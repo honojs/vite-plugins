044a023d63d1ce903aa6bab132b16d0799265766
Aris Riswanto <62107426+arisris@users.noreply.github.com>
2024-08-28 20:21:53 +0700

    feat(dev-server): Add initial nodejs adapter (#167)
    * Initial node adapter
    
    * format
    
    * add changeset

---
+++ packages/dev-server/src/adapter/node.ts
@@ -0,0 +1,19 @@
+import type { Adapter } from '../types'
+export const nodeAdapter = (): Adapter => {
+  if (typeof globalThis.navigator === 'undefined') {
+    // @ts-expect-error not typed well
+    globalThis.navigator = {
+      userAgent: 'Node.js',
+    }
+  } else {
+    Object.defineProperty(globalThis.navigator, 'userAgent', {
+      value: 'Node.js',
+      writable: false,
+    })
+  }
+  return {
+    env: process.env,
+  }
+}
+
+export default nodeAdapter
