fc15f718c0172f84748f8717f53abba40470baed
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-09-15 16:12:59 +0900

    feat(build): add Deno adapter (#181)
    * feat(build): add Deno adapter
    
    * fix package.json
    
    * add changeset
    
    * fix typo
    
    * feat: remove `Deno.serve`

---
+++ packages/build/test/deno.test.ts
@@ -0,0 +1,39 @@
+import { build } from 'vite'
+import { existsSync, readFileSync, rmSync } from 'node:fs'
+import denoBuildPlugin from '../src/adapter/deno'
+
+describe('Build Plugin with Deno Adapter', () => {
+  const testDir = './test/mocks/app-static-files'
+  const entry = './src/server.ts'
+
+  afterAll(() => {
+    rmSync(`${testDir}/dist`, { recursive: true, force: true })
+  })
+
+  it('Should build the project correctly with the plugin', async () => {
+    const outputFile = `${testDir}/dist/index.js`
+
+    await build({
+      root: testDir,
+      plugins: [
+        denoBuildPlugin({
+          entry,
+        }),
+      ],
+    })
+
+    expect(existsSync(outputFile)).toBe(true)
+
+    const output = readFileSync(outputFile, 'utf-8')
+    expect(output).toContain('Hello World')
+    expect(output).toContain('use("/foo.txt"')
+    expect(output).toContain('use("/js/*"')
+
+    const outputFooTxt = readFileSync(`${testDir}/dist/foo.txt`, 'utf-8')
+    expect(outputFooTxt).toContain('foo')
+
+    const outputJsClientJs = readFileSync(`${testDir}/dist/js/client.js`, 'utf-8')
+    // eslint-disable-next-line quotes
+    expect(outputJsClientJs).toContain("console.log('foo')")
+  })
+})
