ead8c3255f2d7fb68084b8d30c3fbe9fcaabb3ec
Yusuke Wada <yusuke@kamawada.com>
2024-12-05 18:38:00 +0900

    fix(build): support latest `hono` (#196)
    * fix(build): support latest `hono`
    
    * add changeset

---
+++ .changeset/strong-sloths-wash.md
@@ -0,0 +1,5 @@
+---
+'@hono/vite-build': patch
+---
+
+fix: support latest `hono`
--- packages/build/test/basic.test.ts
+++ packages/build/test/basic.test.ts
@@ -5,14 +5,13 @@ import buildPlugin from '../src/base'
 describe('Base Plugin', () => {
   const testDir = './test/mocks/app'
   const entry = './src/server.ts'
+  const outputFile = `${testDir}/dist/index.js`
 
   afterAll(() => {
     rmSync(`${testDir}/dist`, { recursive: true, force: true })
   })
 
   it('Should build the project correctly with the plugin', async () => {
-    const outputFile = `${testDir}/dist/index.js`
-
     await build({
       root: testDir,
       plugins: [
@@ -26,5 +25,16 @@ describe('Base Plugin', () => {
 
     const output = readFileSync(outputFile, 'utf-8')
     expect(output).toContain('Hello World')
+  })
+
+  it('Should return correct responses from the output file', async () => {
+    const module = await import(outputFile)
+    const app = module['default']
+
+    let res = await app.request('/')
+    expect(res.status).toBe(200)
+
+    res = await app.request('/not-found')
+    expect(res.status).toBe(404)
   })
 })
