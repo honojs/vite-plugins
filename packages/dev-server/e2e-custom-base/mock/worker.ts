5389cf40f54b1cbfb713dc97d50fe8bd315f4327
n4n5 <its.just.n4n5@gmail.com>
2025-08-22 21:02:12 -0600

    fix tests

--- packages/dev-server/e2e-custom-base/mock/worker.ts
+++ packages/dev-server/e2e-custom-base/mock/worker.ts
@@ -1,6 +1,4 @@
 import { Hono } from 'hono'
-import { getRuntimeKey } from 'hono/adapter'
-import { getConnInfo } from '../../src/conninfo'
 
 const app = new Hono<{
   Bindings: {
@@ -13,88 +11,8 @@ app.get('/', (c) => {
   return c.html('<h1>Hello Vite!</h1>')
 })
 
-app.get('/with-nonce', (c) => {
-  // eslint-disable-next-line quotes -- allowing using double-quotes bc of embedded single quotes
-  c.header('content-security-policy', "script-src-elem 'self' 'nonce-ZMuLoN/taD7JZTUXfl5yvQ==';")
-  return c.html('<h1>Hello Vite!</h1>')
-})
-
-app.get('/name', (c) => c.html(`<h1>My name is ${c.env.NAME}</h1>`))
-
-app.get('/wait-until', (c) => {
-  const fn = async () => {}
-  c.executionCtx.waitUntil(fn())
-  return c.html('<h1>Hello Vite!</h1>')
-})
-
-app.get('/file.ts', (c) => {
-  return c.text('console.log("exclude me!")')
-})
-
 app.get('/app/foo', (c) => {
   return c.html('<h1>exclude me!</h1>')
-})
-
-app.get('/ends-in-ts', (c) => {
-  return c.text('this should not be excluded')
-})
-
-app.get('/favicon.ico', (c) => {
-  return c.text('a good favicon')
-})
-
-app.get('/static/foo.png', (c) => {
-  return c.text('a good image')
-})
-
-app.get('/stream', () => {
-  const html = new TextEncoder().encode('<h1>Hello Vite!</h1>')
-  const stream = new ReadableStream({
-    start(controller) {
-      controller.enqueue(html)
-      controller.close()
-    },
-  })
-  return new Response(stream, {
-    headers: { 'Content-Type': 'text/html', 'x-via': 'vite' },
-  })
-})
-
-// @ts-expect-error the response is string
-app.get('/invalid-response', () => {
-  return '<h1>Hello!</h1>'
-})
-
-app.get('/invalid-error-response', (c) => {
-  try {
-    // @ts-expect-error the variable does not exist, intentionally
-    doesNotExist = true
-
-    return c.html('<h1>Hello Vite!</h1>')
-  } catch (err) {
-    return err
-  }
-})
-
-app.get('/env', (c) => {
-  return c.json({ env: c.env })
-})
-
-app.get('/cache', async (c) => {
-  const myCache = await caches.open('myCache')
-  const res = await myCache.match(c.req.url)
-  if (res) {
-    return res
-  }
-  await myCache.put(
-    c.req.url,
-    new Response('cached', {
-      headers: {
-        'Cache-Control': 's-maxage=10',
-      },
-    })
-  )
-  return c.text('first')
 })
 
 export default app
